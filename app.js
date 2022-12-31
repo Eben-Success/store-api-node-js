ea5984f3515a90a56e91e47af8932e42bca7d742
Eben-Success <aebenezer237@gmail.com>
2022-12-31 17:05:19 +0000

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect')
 
 const notFoundMiddleware = require('./middleware/not-Found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -26,6 +27,7 @@ const port = process.env.PORT || 3000
 const start = async ()=>{
   try{
 // connectDG
+await connectDB(process.env.MONGO_URI)
 app.listen(port, console.log(`Server is listening on port ${port}...`))
   } catch (error) {
     console.log(error);
